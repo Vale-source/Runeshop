445bc838af898fa8b316d2b821be002cfb679b2b
Valentin Curiel <valentincuriel03@gmail.com>
2025-05-31 19:51:33 -0300

    feat: Se agrego el controlador para borrar imagen

--- Runeshop Ecommerce/src/main/java/com/example/runeshop_ecommerce/controllers/ImagenController.java
+++ Runeshop Ecommerce/src/main/java/com/example/runeshop_ecommerce/controllers/ImagenController.java
@@ -3,14 +3,34 @@ package com.example.runeshop_ecommerce.controllers;
 import com.example.runeshop_ecommerce.entities.Imagen;
 import com.example.runeshop_ecommerce.services.BaseService;
 import com.example.runeshop_ecommerce.services.ImagenService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.awt.*;
 
 @RestController
 @RequestMapping("/imagen")
 public class ImagenController extends BaseController<Imagen, Long> {
-
-    public ImagenController(ImagenService imagenService) {
+    
+    private final ImagenService imagenService;
+    
+    public ImagenController(ImagenService imagenService, ImagenService imagenService1) {
         super(imagenService);
+        this.imagenService = imagenService1;
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteString(
+            @PathVariable Long id
+            ) throws Exception {
+        try {
+            imagenService.borrarImagen(id);
+            return ResponseEntity.status(200).body("La imagen se borró con éxito");
+        } catch (Exception e) {
+            throw new Exception(e.getMessage());
+        }
     }
 }
