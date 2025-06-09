50c7bd77e4235fb2c99f4a847a0f94578d588938
Valentin Curiel <valentincuriel03@gmail.com>
2025-06-09 17:00:05 -0300

    feat: Se mejoro el manejo de minimo y maximo del filtro

--- Runeshop Ecommerce/src/main/java/com/example/runeshop_ecommerce/services/ProductoService.java
+++ Runeshop Ecommerce/src/main/java/com/example/runeshop_ecommerce/services/ProductoService.java
@@ -72,8 +72,17 @@ public class ProductoService extends BaseService<Producto, Long> {
             Pageable pageable,
             String orden
     ) throws Exception {
-        if (min != null && max != null && min > max) {
-            throw new Exception("El valor de 'min' no puede ser mayor que 'max'.");
+        
+        if (min != null && max != null) {
+            throw new Exception("'max' y 'min' no pueden ser null");
+        }
+
+        if (min > max) {
+            throw new Exception("El valor de 'min' no puede ser mayor que 'max'");
+        }
+
+        if (min <= 0.0) {
+            throw new Exception("El valor de 'min' y 'max' no puede ser 0");
         }
 
         if (orden.equalsIgnoreCase("desc")) {
