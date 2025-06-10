c38808c0a934b7936079a696147ba388d1e34acb
Valentin Curiel <valentincuriel03@gmail.com>
2025-06-10 00:45:22 -0300

    feat: Se agrego para ordenar los precios teniendo en cuenta el descuento

--- Runeshop Ecommerce/src/main/java/com/example/runeshop_ecommerce/repositories/ProductoRepository.java
+++ Runeshop Ecommerce/src/main/java/com/example/runeshop_ecommerce/repositories/ProductoRepository.java
@@ -17,6 +17,7 @@ public interface ProductoRepository extends BaseRepository<Producto, Long> {
     @Query("SELECT p FROM Producto p ORDER BY p.modelo ASC")
     Page<Producto> getProductoPaginado(Pageable pageable);
 
+
     @Query("""
     SELECT p
     FROM Producto p
@@ -26,12 +27,16 @@ public interface ProductoRepository extends BaseRepository<Producto, Long> {
     AND (:marcaParam IS NULL OR d.marca IN :marcaParam)
     AND (:talleParam IS NULL OR t.numero IN :talleParam)
     AND (:tipoProdParam IS NULL OR p.tipoProducto IN :tipoProdParam)
-    AND (:nombreParam IS NULL OR :nombreParam = p.modelo)
+    AND (:nombreParam IS NULL OR p.modelo LIKE UPPER(CONCAT(:nombreParam, '%')))
     AND (:categoriaParam IS NULL OR p.categoria.nombre IN :categoriaParam)
     AND ((:min IS NULL AND :max IS NULL)
     OR (:min IS NOT NULL AND :max IS NOT NULL AND d.precio.precioVenta BETWEEN :min AND :max)
     OR (:min IS NOT NULL AND :max IS NOT NULL AND d.precioDescuento IS NOT NULL AND d.precioDescuento BETWEEN :min AND :max))
-    ORDER BY d.precio.precioVenta DESC
+    ORDER BY 
+    CASE 
+        WHEN d.precioDescuento IS NOT NULL THEN d.precioDescuento
+        ELSE d.precio.precioVenta
+    END ASC
     """)
     Page<Producto> filtrarConPaginadoAsc(
             @Param("sexoParam") String sexo,
@@ -54,12 +59,16 @@ public interface ProductoRepository extends BaseRepository<Producto, Long> {
     AND (:marcaParam IS NULL OR d.marca IN :marcaParam)
     AND (:talleParam IS NULL OR t.numero IN :talleParam)
     AND (:tipoProdParam IS NULL OR p.tipoProducto IN :tipoProdParam)
-    AND (:nombreParam IS NULL OR :nombreParam = p.modelo)
+    AND (:nombreParam IS NULL OR p.modelo LIKE UPPER(CONCAT(:nombreParam, '%')))
     AND (:categoriaParam IS NULL OR p.categoria.nombre IN :categoriaParam)
     AND ((:min IS NULL AND :max IS NULL)
     OR (:min IS NOT NULL AND :max IS NOT NULL AND d.precio.precioVenta BETWEEN :min AND :max)
     OR (:min IS NOT NULL AND :max IS NOT NULL AND d.precioDescuento IS NOT NULL AND d.precioDescuento BETWEEN :min AND :max))
-    ORDER BY d.precio.precioVenta DESC
+    ORDER BY
+    CASE 
+        WHEN d.precioDescuento IS NOT NULL THEN d.precioDescuento
+        ELSE d.precio.precioVenta
+    END DESC 
     """)
     Page<Producto> filtrarConPaginadoDesc(
             @Param("sexoParam") String sexo,
